775b3db9a2948330f24356ba779bcc47b38e1ee0
Ethan Crann <ecrann@protonmail.com>
2024-07-24 13:17:31 -0230

    Egg enemy added with walking animation

---
+++ scenes/evil_egg.gd
@@ -0,0 +1,50 @@
+extends CharacterBody3D
+class_name EvilEgg
+
+#@export var base_movement_speed: float = 0.5;
+@export var max_movement_speed: float = 2;
+@export var added_accel: float = 0.05;
+var current_movement_speed: float;
+var movement_target_position: Vector3 = Vector3(-3.0,0.0,2.0)
+
+@export var animation_speed_curve: Curve;
+#@export var egg_mesh: EggMesh;
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+
+func _ready():
+	current_movement_speed = 0;
+	call_deferred("actor_setup")
+
+func actor_setup():
+	await get_tree().physics_frame
+	set_movement_target(movement_target_position)
+
+func set_movement_target(movement_target: Vector3):
+	movement_target.y = position.y 
+	look_at(movement_target);
+	navigation_agent.set_target_position(movement_target)
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		velocity = Vector3(0,0,0);
+		current_movement_speed = 0; 
+		#egg_mesh.reset_animations();
+		move_and_slide();
+	else:
+		var current_agent_position: Vector3 = global_position
+		var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+		current_movement_speed += added_accel;
+		var animation_speed = animation_speed_curve.sample(current_movement_speed/max_movement_speed);
+		#if not egg_mesh.animation_playing():
+			#egg_mesh.play_animation("Walkin",animation_speed);
+		#else:
+			#egg_mesh.set_animation_speed(animation_speed);
+		if current_movement_speed > max_movement_speed:
+			current_movement_speed = max_movement_speed;
+		velocity = current_agent_position.direction_to(next_path_position) * current_movement_speed
+		move_and_slide()
+
+
+func _on_main_bb_egg_location(location):
+	set_movement_target(location)
+	pass # Replace with function body.
--- scenes/main.gd
+++ scenes/main.gd
@@ -4,6 +4,7 @@ extends Node3D
 @onready var bb_egg := $bb_egg
 #@onready var player := $Player
 @onready var player := $PlayerFlat
+signal bb_egg_location(location: Vector3)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,4 +15,5 @@ func _process(delta):
 	var camera_pos = player.get_camera_position();
 	camera_pos.y = 0;
 	bb_egg.set_movement_target(camera_pos);
+	bb_egg_location.emit(bb_egg.position);
 	pass
