a8a1ae96358cda577f96e96f904d815381feb66e
Ethan Crann <ecrann@protonmail.com>
2024-07-26 21:35:15 -0230

    Decal + Particles on egg splat

--- scenes/ProjectileEgg.gd
+++ scenes/ProjectileEgg.gd
@@ -13,13 +13,24 @@ var b_total = 0;
 
 @onready var egg_mesh = $Sketchfab_Scene/Sketchfab_model/egg_fbx/RootNode/Sphere/Sphere_DefaultMaterial_0;
 @onready var splat_sound := $SplatSound;
+
 @onready var splat_particles := $GPUParticles3D;
+@onready var decal := $Decal
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
 	set_as_top_level(true);
+	
+func egg_explode():
+	decal.set_as_top_level(true);
+	splat_particles.set_as_top_level(true);
+	decal.rotation = Vector3(0,0,0);
+	splat_particles.rotation = Vector3(0,0,0);
+	
 
 func _on_body_entered(body):
-	splat_sound.play()
+	splat_sound.play();
+	animation_player.play("Explode");
 	if body.is_in_group("Enemy"):
 		body.health -= DAMAGE
 		queue_free()
